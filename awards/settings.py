6699397b62ec1b3ce4e4fa31d858b52a67bf7542
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-18 20:59:15 +0300

    Seeting up cloudinary to store images

--- awards/settings.py
+++ awards/settings.py
@@ -10,7 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+import dj_database_url
+from decouple import config, Csv
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+import django_heroku
 from pathlib import Path
+
+
+cloudinary.config( 
+  cloud_name = "dqvarhwzn", 
+  api_key = "811246556949398", 
+  api_secret = "YE5NEj5BxfH8Uezhhx1MgbYSoqU",
+  secure = True
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
