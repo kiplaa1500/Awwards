7a4da0d5461c5cf7909afd7b12a2d270c3b0e1d1
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-18 15:47:05 +0300

    View function to post a new project

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 import datetime as dt
 from .models import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -19,3 +20,19 @@ def get_project_by_id(request, id):
         raise Http404()    
     
     return render(request, "project.html", {"project":project})
+# view funtion to create new project
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.author = current_user
+            form.save()
+        return redirect('/')
+
+    else:
+        form = NewProjectForm()
+    return render(request, 'new_project.html', {"form":form})
+
