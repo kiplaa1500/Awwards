ad9b66e7929e9257cfb221981a4b596fd10cd20c
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-18 15:43:16 +0300

    View funtion to return home page

--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,get_object_or_404
+import datetime as dt
 # Create your views here.
+
+def home(request):
+    date = dt.date.today()
+    projects = Projects.objects.all()
+    return render(request, 'index.html', {"date": date,"projects":projects})
