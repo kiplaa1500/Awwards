4fde3e11d12b27dc9d91b5b7113f8688cc1bb827
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-20 17:42:23 +0300

    Adding Readme and License

--- user/forms.py
+++ user/forms.py
@@ -17,3 +17,20 @@ class ProfileUpdateForm(forms.ModelForm):
         widgets = {
           'bio': forms.Textarea(attrs={'rows':2, 'cols':10,}),
         }
+
+
+# Updating user 
+class UpdateUser(forms.ModelForm):
+  email = forms.EmailField()
+
+  class Meta:
+    model = User
+    fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+        widgets = {
+          'bio': forms.Textarea(attrs={'rows':2, 'cols':10,}),
+        }
